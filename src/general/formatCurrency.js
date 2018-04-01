5f7bdd12b1edef4fde69a085b60e63def088e63b
Bruno Ravanhani <bruno.ravanhani@outlook.com>
2018-04-01 09:17:08 -0400

    Fix currency format bug

--- src/general/formatCurrency.js
+++ src/general/formatCurrency.js
@@ -5,6 +5,14 @@ export const formatToString = (value) => {
 
   var valueStr = value.toString().replace('.', ',');
 
+  var splitted = valueStr.split(',');
+
+  if (splitted.length === 1) {
+    valueStr += ',00';
+  } else if (splitted.length === 2 && splitted[1].length === 1) {
+    valueStr += '0';
+  }
+
   return `R$ ${valueStr}`;
 
 }
--- src/general/formatCurrency.test.js
+++ src/general/formatCurrency.test.js
@@ -8,6 +8,10 @@ describe('formatToString', () => {
 
   test('Expect to be a empty string when 0', () => expect(formatToString(0)).toBe(''));
 
-  test('', () => expect(formatToString(4.99)).toBe('R$ 4,99'));
+  test('Expect to be the formatted value when integer number', () => expect(formatToString(4)).toBe('R$ 4,00'));
+
+  test('Expect to be the formatted value ', () => expect(formatToString(4.99)).toBe('R$ 4,99'));
+
+  test('Expect to be the formatted value with 0 in final', () => expect(formatToString(4.9)).toBe('R$ 4,90'));
 
 });
