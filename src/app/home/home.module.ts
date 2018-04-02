57f747ae293cf4c46c5c83febc469c15851b67ea
Cristiano Teixeira <cristiano_teiixeira@hotmail.com>
2018-04-02 10:35:01 -0400

    Style Guide

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { HomeComponent } from "./home/home.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [CommonModule],
