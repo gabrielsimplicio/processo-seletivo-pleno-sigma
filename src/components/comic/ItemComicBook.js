4563659a502aa56c60350369e537e13a86ab9a09
Diogo Sousa Borges <diogosousa2266@gmail.com>
2018-04-04 04:48:36 -0400

    Commit correção item

--- src/components/comic/ItemComicBook.js
+++ src/components/comic/ItemComicBook.js
@@ -2,24 +2,6 @@ import React, { Component } from 'react';
 
 import { Link } from 'react-router-dom';
 
-/*
-        <figure className="imgThumb"><img src={this.props.urlImage} alt={this.props.title}/>
-                    <figcaption>
-                        <h6>{this.props.title}</h6>
-                        <br></br>
-                        <p>Price: ${this.props.price}</p>
-                        <div className="icons">
-                            <Link title="View Comic Detail" to={'/comics/' + this.props.idComic}><i className="material-icons">remove_red_eye</i></Link>
-                            <a title="Add Comic to Cart" href='#!' onClick={this.props.actionAddCart}><i className="material-icons">add_shopping_cart</i></a>
-                        </div>
-                    </figcaption>
-                </figure>
-*/
-
-
-/* */
-
-
 class ItemComicBook extends Component {
     render() {
         return (
